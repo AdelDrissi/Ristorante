1b2849b9b6ac288ccf83432d8202bb25214da231
Adel <leda91@hotmail.fr>
2023-09-26 22:41:03 +0200

    file Navbar.js done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,70 @@
+import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+// import logo from '../assets/logo/logo2.png';
+import hamburgerMenu from '../assets/icons/more-M.png';
+
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-}
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    console.log(isMenuOpen);
+  };
 
+  return (
+    <header className="navbar">
+      <div className="navbar__logo">
+        {/* <NavLink to="/">
+          <img src={logo} alt="revenir à la page d'accueil"></img>
+        </NavLink> */}
+      </div>
+      <nav
+        className={`navbar__container ${
+          isMenuOpen ? 'navbar__container--open menuAppear' : ''
+        }`}
+      >
+        <ul className="navbar__container__list">
+          <li>
+            <NavLink
+              to="/"
+              className={'navlinks'}
+              tabIndex="0"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Accueil
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/Services"
+              className={'navlinks'}
+              tabIndex="0"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Menu
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/Contact"
+              className={'navlinks'}
+              tabIndex="0"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Reservation
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className="navbar__hamburgerMenu">
+        <img
+          src={hamburgerMenu}
+          alt="Ouvrir le Menu de navigation"
+          onClick={toggleMenu}
+        ></img>
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
